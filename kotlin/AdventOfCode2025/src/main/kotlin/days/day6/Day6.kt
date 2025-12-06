08ea37c65b0ed5ce32cf98099f49f4ff59174a18
ckainz11 <christoph.kainz11@gmail.com>
2025-12-06 12:41:19 +0100

    refactor day6 in kotlin

--- kotlin/AdventOfCode2025/src/main/kotlin/days/day6/Day6.kt
+++ kotlin/AdventOfCode2025/src/main/kotlin/days/day6/Day6.kt
@@ -5,40 +5,27 @@ import util.Matrix
 import util.allLongs
 import util.columns
 import util.join
-import util.rotated
+import util.split
 
 class Day6(override val input: String) : Day<Long>(input) {
 
-    private val columns = input.lines().dropLast(1).map { it.allLongs() }.columns()
     private val operations = input.lines().last().filter { !it.isWhitespace() }.map { it.toOperation() }
 
-    override fun solve1(): Long = columns.zip(operations)
+    override fun solve1(): Long = input.lines().dropLast(1)
+        .map { line -> line.allLongs() }
+        .columns()
+        .solve()
+
+    override fun solve2(): Long = input.lines().dropLast(1)
+        .map { line -> line.toList() }
+        .columns(pad = ' ')
+        .split { col -> col.all { it.isWhitespace() } }
+        .map { problem -> problem.map { col -> col.join().trim().toLong() } }
+        .solve()
+
+    private fun List<List<Long>>.solve(): Long = this
+        .zip(operations)
         .sumOf { (column, op) -> column.reduce(op) }
-
-    override fun solve2(): Long {
-        val test = input.lines().dropLast(1).map { it.toList() }.columns(' ')
-        var sum = 0L
-        var problemCount = 0;
-        val nums = mutableListOf<Long>()
-        for (col in test)  {
-            if (col.all { it.isWhitespace() }) {
-                sum += nums.reduce(operations[problemCount])
-                problemCount++
-                nums.clear()
-                continue
-            }
-
-            val num = col.join("").trim().toLong()
-            nums.add(num)
-        }
-        sum += nums.reduce(operations[problemCount])
-        return sum
-    }
-
-    private fun List<Long>.transformRightToLeft(): List<Long> {
-        val maxLength = this.maxOf { it.toString().length }
-        return emptyList()
-    }
 
     private fun Char.toOperation(): (Long, Long) -> Long = when(this) {
         '*' -> Long::times
--- kotlin/AdventOfCode2025/src/main/kotlin/util/CollectionUtils.kt
+++ kotlin/AdventOfCode2025/src/main/kotlin/util/CollectionUtils.kt
@@ -3,20 +3,44 @@ package util
 /*------ pairwise ------*/
 
 fun <T> List<T>.pairwise() = mapIndexed { index, first -> drop(index + 1).map { second -> first to second } }.flatten()
-fun <T, R> List<T>.pairwise(transform: (T, T) -> R) = mapIndexed { index, first -> drop(index + 1).map { second -> transform(first, second) } }.flatten()
+fun <T, R> List<T>.pairwise(transform: (T, T) -> R) =
+    mapIndexed { index, first -> drop(index + 1).map { second -> transform(first, second) } }.flatten()
 
 /*------ middle ------*/
 
 fun <T> List<T>.middle(): T {
-	if (size % 2 == 0) error("List must have an odd number of elements")
-	return this[size / 2]
+    if (size % 2 == 0) error("List must have an odd number of elements")
+    return this[size / 2]
 }
 
 /*----- permutations -----*/
 
 fun <T> List<T>.permutations(): List<List<T>> {
-	return if (this.size == 1) listOf(this)
-	else this.flatMap { i -> (this - i).permutations().map { listOf(i) + it } }
+    return if (this.size == 1) listOf(this)
+    else this.flatMap { i -> (this - i).permutations().map { listOf(i) + it } }
+}
+
+/*----- split -----*/
+
+/**
+ * Splits the list into sublists each time the predicate is true.
+ * The element that matches the predicate is not included in the resulting sublists.
+ * This may cause empty sublists in the result.
+ */
+fun <T> List<T>.split(predicate: (T) -> Boolean): List<List<T>> {
+    val result = mutableListOf<MutableList<T>>()
+    var group = mutableListOf<T>()
+    for (item in this) {
+        if (!predicate(item))
+            group.add(item)
+        else {
+            result.add(group)
+            group = mutableListOf()
+        }
+    }
+
+    result.add(group)
+    return result
 }
 
 /*----- String Joining -----*/
