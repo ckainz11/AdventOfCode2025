28d39bdc83346e2304a30be799e91a0760802ffb
ckainz11 <christoph.kainz11@gmail.com>
2025-12-09 20:12:41 +0100

    solve day 9 part 2 in kotlin

--- kotlin/AdventOfCode2025/src/main/kotlin/days/day9/Day9.kt
+++ kotlin/AdventOfCode2025/src/main/kotlin/days/day9/Day9.kt
@@ -4,9 +4,11 @@ import setup.Day
 import util.Point
 import util.allInts
 import util.subsetsOfSize
-import kotlin.math.abs
+import kotlin.math.min
+import kotlin.math.max
 
 class Day9(override val input: String) : Day<Long>(input) {
+
 
     private val redTiles = input.lines()
         .map { line -> line.allInts().let { (x, y) -> Point(x, y) } }
@@ -16,16 +18,39 @@ class Day9(override val input: String) : Day<Long>(input) {
         .map { (a, b) -> Rectangle(a, b) }
         .sortedByDescending { it.area }
 
+    private val edges = redTiles
+        .plus(redTiles.first())
+        .zipWithNext { a, b -> Rectangle(a, b) }
+        .toSet()
+
     override fun solve1(): Long = rectangles.first().area
 
-    override fun solve2(): Long {
-        TODO("Not yet implemented")
-    }
+    override fun solve2(): Long = rectangles.first { it.isValid(edges) }.area
 
-    data class Rectangle(val corner1: Point, val corner2: Point) {
-        val width: Long = (abs(corner2.x - corner1.x) + 1).toLong()
-        val height: Long = (abs(corner2.y - corner1.y) + 1).toLong()
-        val area: Long = width * height
+    data class Rectangle(val xRange: LongRange, val yRange: LongRange) {
 
+        constructor(p1: Point, p2: Point) : this(
+            xRange = min(p1.x, p2.x).toLong()..max(p1.x, p2.x).toLong(),
+            yRange = min(p1.y, p2.y).toLong()..max(p1.y, p2.y).toLong(),
+        )
+
+        val width: Long get() = if (xRange.isEmpty()) 0 else xRange.last - xRange.first + 1
+        val height: Long get() = if (xRange.isEmpty()) 0 else yRange.last - yRange.first + 1
+        val area: Long get() = width * height
+
+        // check if all corners are inside the polygon created by red tiles
+        fun isValid(edges: Set<Rectangle>): Boolean {
+            val inside = Rectangle(
+                xRange = (xRange.first + 1)..<xRange.last,
+                yRange = (yRange.first + 1)..<yRange.last,
+            )
+            return edges.none { it overlaps inside }
+        }
+
+        infix fun Rectangle.overlaps(other: Rectangle): Boolean {
+            val horizontalOverlap = maxOf(xRange.first, other.xRange.first) <= minOf(xRange.last, other.xRange.last)
+            val verticalOverlap = maxOf(yRange.first, other.yRange.first) <= minOf(yRange.last, other.yRange.last)
+            return horizontalOverlap && verticalOverlap
+        }
     }
 }
